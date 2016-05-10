64d9e5429ebaf79f98955ec7b710f79afbd7dc1e
Devanté <devante.wilson@outlook.com>
2016-05-10 00:44:57 -0400

    Create FromMeToYou.java

---
+++ Grade-12/Java/FromMeToYou.java
@@ -0,0 +1,18 @@
+/**
+ Author: Devante Wilson
+ Date Created: 2014 Feb 04
+ */
+
+public class FromMeToYou
+{
+  public static void main(String args[])
+  {
+      System.out.println("From: Bill Smith");
+      System.out.println("Address: Dell Computer, Bldg 13");
+      System.out.println("Date: April 12, 2005");
+      System.out.println("");
+      System.out.println("To: Jack Jones");
+      System.out.println("");
+      System.out.println("Help! I'm trapped inside a computer");
+  }
+}
